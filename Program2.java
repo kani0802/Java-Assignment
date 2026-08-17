4105791376b56dc2d0193f6ceac603f93dc95164
kani0802 <ch.kanishka0802@gmail.com>
2026-08-17 13:51:08 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -1,14 +1,8 @@
-/*
-Write a Java program that: 
-• Stores an integer value for temperature in Celsius. 
-• Converts it to Fahrenheit using the formula F = (C × 9/5) + 32. 
-• Displays both Celsius and Fahrenheit values. 
-*/
 public class Program2
 {
 	public static void main(String args[])
 	{
-		int celsius = 18;
+		int celsius = 20;
         	double fahrenheit = (celsius * 9.0 / 5) + 32;
         	System.out.println("Celsius: " + celsius);
         	System.out.println("Fahrenheit: " + fahrenheit);
