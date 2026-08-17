a8d63f1716b3775c378af14143328ddb194e89cf
kani0802 <ch.kanishka0802@gmail.com>
2026-08-17 13:49:46 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,17 +1,15 @@
-/*
-Write a program that declares variables for a student’s name, roll number, and marks in three subjects. Assign values and print them. Ensure all variable names follow Java identifier rules.
-*/
+
 public class Program1
 {
 public static void main(String args[])
 {
 int roll_no,Science,Maths,English;
 String name;
-Science=80;
+Science=90;
 Maths=70;
 English=96;
-name="Kanika";
-roll_no=18;
+name="Kanishka";
+roll_no=5;
 System.out.println("Name="+name);
 System.out.println("Roll no="+roll_no);
 System.out.println("Science="+Science);
